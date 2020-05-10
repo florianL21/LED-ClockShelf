7ca4c5ae998c3a3d1a9d3c037605bb76db2b3e0e
Florian Laschober <flo.laschober@gmail.com>
2020-05-10 12:54:25 +0200

    Added demo code to make testing of animations easier

--- lib/TimeManager/TimeManager.cpp
+++ lib/TimeManager/TimeManager.cpp
@@ -5,6 +5,9 @@ TimeManager::TimeManager(long gmtOffset_sec, int daylightOffset_sec, const char*
 	configTime(gmtOffset_sec, daylightOffset_sec, server1);
 	lastUpdate = millis();
 	updateInterv = updateIntervall;
+	currentTime.hours = 0;
+	currentTime.minutes = 0;
+	currentTime.seconds = 0;
 }
 
 TimeManager::~TimeManager()
@@ -30,6 +33,7 @@ void TimeManager::update()
 	if(lastUpdate + updateInterv < millis())
 	{
 		lastUpdate = millis();
+		// Time code, use this for normal operation
 		struct tm timeinfo;
 		if(!getLocalTime(&timeinfo))
 		{
@@ -42,5 +46,20 @@ void TimeManager::update()
 		currentTime.hours = timeinfo.tm_hour;
 		currentTime.minutes = timeinfo.tm_min;
 		currentTime.seconds = timeinfo.tm_sec;
+
+		//DEMO CODE: Useful for testing animations
+		//Tipp: if you have a bad internet connection disable Blynk functionallity as this will mess with the timing on fast updating digits
+		/*
+		currentTime.minutes++;
+		if(currentTime.minutes > 59)
+		{
+			currentTime.hours++;
+			currentTime.minutes = 0;
+		}
+		if(currentTime.hours > 24)
+		{
+			currentTime.hours = 0;
+		}
+		*/
 	}
 }
